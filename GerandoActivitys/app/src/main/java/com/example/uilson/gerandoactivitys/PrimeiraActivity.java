d23f189fc011e811dae6ce7b79d8fbd4741ca29d
Uilson Claudio Filho <uilson.filho@gmail.com>
2016-11-13 23:42:25 -0200

    TEETE
    TETE

---
+++ GerandoActivitys/app/src/main/java/com/example/uilson/gerandoactivitys/PrimeiraActivity.java
@@ -0,0 +1,13 @@
+package com.example.uilson.gerandoactivitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira);
+    }
+}
